cf97ae22d1aa05042abf53050d960e96442d758f
Abisagy Nafula <abisakyn@gmail.com>
2024-05-31 23:05:16 +0300

    nodified

--- app/models/users/user.js
+++ app/models/users/user.js
@@ -1,1 +1,25 @@
 const mongoose = require('mongoose');
+
+const Schema =mongoose.Schema;
+
+const userSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    isAdmin:{
+        type:Boolean,
+        required:true,
+        default:false
+    }
+})
+
+module.exports=mongoose.model("User",userSchema);
