52e8b7f3ea61039a17a634b8db8d08e3497f235a
Abisagy Nafula <abisakyn@gmail.com>
2024-06-01 13:32:23 +0300

    modified

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const router = require('./routes/routes');
 
 dotenv.config();
 
 const app = express();
 
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use('/api/', router);
 
-const port =process.env.PORT ||3000
+const port = process.env.PORT || 3000;
 
-const getAllTours = (req, res) =>{
-    res.status(200).json({data:{
-        tours
-    }})
-}
+const getAllTours = (req, res) => {
+    res.status(200).json({ data: { tours } });
+};
 
-app.get('/api/tours', getAllTours)
+app.get('/api/tours', getAllTours);
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log('Connected to DB');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
