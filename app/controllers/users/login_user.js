ec5283f2bc74c0ebad595d83d9ec3e7551cb8651
Abisagy Nafula <abisakyn@gmail.com>
2024-06-02 23:11:47 +0300

    added more verification

--- app/controllers/users/login_user.js
+++ app/controllers/users/login_user.js
@@ -1,10 +1,14 @@
 const { compareSync } = require('bcryptjs')
 const User = require('../../models/users/user')
+const AppError =require('../../utils/appError')
 
 exports.loginUser = async (req, res)=>{
     const {email,password}=req.body
 
     try{
+        if(!email ||!password){
+            next(new AppError('please enter email and password',400))
+        }
         const user = await User.findOne({ email: email, isVerified: true });
         if (!user) {
             return res.status(404).json({ error: 'User not found or not verified' });
