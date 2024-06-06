100a65e3da393b61d11d893b2250472d8bfa594a
Abisagy Nafula <abisakyn@gmail.com>
2024-06-06 23:22:03 +0300

    modified

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,8 @@ const {  signup } = require('../app/controllers/users/create_user');
 const { verifyEmail } = require('../app/controllers/users/verify_email');
 const { loginUser } = require('../app/controllers/users/login_user');
 const { AddTours } = require('../app/controllers/tours/add_tours');
-const {protect}=require('../app/controllers/users/protect')
+const {protect,restrictTo}=require('../app/controllers/users/protect');
+const { forgotPassword } = require('../app/controllers/users/forgot_password');
 
 
 const router = express.Router();
@@ -11,6 +12,7 @@ const router = express.Router();
 router.post('/create/user', signup);
 router.post('/verifyEmail',verifyEmail);
 router.post('/login',loginUser)
-router.post('/add-tours',AddTours)
+router.post('/forgot-password',forgotPassword)
+router.post('/add-tours',protect,restrictTo('admin'), AddTours)
 
 module.exports = router;
