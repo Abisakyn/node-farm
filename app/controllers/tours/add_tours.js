9e8af8e66ce02faed7776a8dac4f0cd6b294e94b
Abisagy Nafula <abisakyn@gmail.com>
2024-06-21 13:21:56 +0300

    modified the add tours controller

--- app/controllers/tours/add_tours.js
+++ app/controllers/tours/add_tours.js
@@ -1,14 +1,17 @@
 const Tours = require('../../models/tours/tours');
 
 exports.AddTours = async (req, res, next) => {
-    const { name } = req.body; // Destructure the name property from req.body
+    const { name,image,nameOfTheMovie,location } = req.body; 
     try {
         const tour = await Tours.findOne({ name: name });
         if (tour) {
             return res.status(400).json({ message: 'Tour already exists' });
         }
         const newTour = new Tours({
-            name: name
+            name: name,
+            image:image,
+            nameOfTheMovie:nameOfTheMovie,
+            location:location
         });
         await newTour.save();
         return res.status(200).json({
