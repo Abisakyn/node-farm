407d8e405b2672134c0e9778e947453d2c6a11c2
Abisagy Nafula <abisakyn@gmail.com>
2024-07-10 09:17:10 +0300

    restructured

---
+++ serverside/app/controllers/users/login_user.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../models/users/user');
+const AppError = require('../../utils/appError');
+
+exports.loginUser = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return next(new AppError('Please enter email and password', 400));
+        }
+
+        const user = await User.findOne({ email }).select('+password');
+
+        if (!user || !(await user.correctPassword(password, user.password))) {
+            return next(new AppError('Password or email is incorrect', 401));
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_IN
+        });
+
+        const cookieOptions = {
+            expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+            httpOnly: true, // cookie cannot be accessed or modified in any way by the browser
+            secure: false, // only accept https
+            sameSite: 'none'
+        };
+
+        res.cookie('jwt', token, cookieOptions);
+
+        //remove the password 
+        user.password = undefined;
+
+        return res.json({
+            status: 'success',
+            token: token
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
---
+++ serverside/app/models/users/user.js
@@ -0,0 +1,104 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcryptjs = require('bcryptjs');
+const crypto = require('crypto');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please enter a valid email']
+    },
+    photo: {
+        type: String,
+        required: false
+    },
+    role:{
+        type: String,
+        enum:['user','guide','lead-guide','admin'],
+        default: 'user'
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        select: false
+    },
+    passwordConfirm: {
+        type: String,
+        required: true,
+        minlength: 8,
+        validate: {
+            validator: function(value) {
+                return value === this.password;
+            },
+            message: 'Passwords are not the same'
+        }
+    },
+    passwordChangedAt:Date,
+    passwordResetToken: {
+        type: String,
+        required: false
+    },
+    passwordResetExpires: Date,
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    isVerified: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+
+    this.password = await bcryptjs.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
+});
+
+userSchema.pre('save',function(next){
+    if(!this.isModified('password')|| this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() - 1000; //sometime it takes long to send the token thats why we subtract one second to cater for the delaay
+    next();
+})
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+    return await bcryptjs.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+    if (this.passwordChangedAt) {
+        console.log(this.passwordChangedAt,JWTTimestamp)
+        const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+        console.log(changedTimestamp,JWTTimestamp)
+        return JWTTimestamp < changedTimestamp;
+}
+//false means not changed
+return false;
+}
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.passwordResetToken= crypto.createHash('sha256').update(resetToken).digest('hex');
+    this.passwordResetExpires=Date.now()+10 * 60 *1000
+    console.log({resetToken},this.passwordResetToken)
+
+    return resetToken;
+};
+
+
+module.exports = mongoose.model('User', userSchema);
